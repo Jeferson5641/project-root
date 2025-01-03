756447dc89f94cdbf70905ddb51787f882107521
Jeferson Gomes da Silva <jefe.gomes154@gmail.com>
2025-01-03 20:53:31 -0300

    implementações nos arquivos app.module e na main para ratas e o swagger

--- auth-services/src/main.ts
+++ auth-services/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth Service')
+    .setDescription('The Auth Service API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3001);
+  console.log('Auth-Service is running on http://localhost:3001');
+  console.log('Auth-Service is running on http://localhost:3001/api-docs');
 }
 bootstrap();
