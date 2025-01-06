97492379927ac76f89df95f4d1a8e22ea538311a
Jeferson Gomes da Silva <jefe.gomes154@gmail.com>
2025-01-06 10:40:05 -0300

    criação da entidade

---
+++ data-service/src/common/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
