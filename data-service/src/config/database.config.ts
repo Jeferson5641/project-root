dfd8bd603df2aca1df2709077f3e629d6611b4cb
Jeferson Gomes da Silva <jefe.gomes154@gmail.com>
2025-01-06 10:48:56 -0300

    criação do documento de configuração para a comunicaçãocom o banco de dados em TS

---
+++ data-service/src/config/database.config.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { User } from "src/common/entities/user.entity";
+
+export const getDatabaseconfig = (ConfigService: ConfigService): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    host: ConfigService.get('DB_HOST'),
+    port: ConfigService.get('DB_PORT'),
+    username: ConfigService.get('DB_USERNAME'),
+    password: ConfigService.get('DB_PASSWORD'),
+    database: ConfigService.get('DB_NAME'),
+    entities: [User], //estáva dessa maneira antes: [__dirname + '/../**/*.entity{.ts,.js}']
+    synchronize: true, // de forma alguma deve ser usado em Produção
+    // logging: true, // como indicação da propria tabnine para o documento eu o coloquei, mas não está ativado porque no documento
+    // não existe essa propriedade
+})
