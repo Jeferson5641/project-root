105179551c0afd62b3db856546aa6d1ce358a1dc
Jeferson Gomes da Silva <jefe.gomes154@gmail.com>
2025-02-22 11:04:37 -0300

    update: alterações feitas nas configurações de comunicação com o banco de dados e alterações no versionamento de algumas libs

--- data-service/src/app.module.ts
+++ data-service/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { getDatabaseConfig } from './config/database.config';
 import { UsersModule } from './modules/users/users.module';
 
@@ -13,8 +13,8 @@ import { UsersModule } from './modules/users/users.module';
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      inject: [ConfigService],
       useFactory: getDatabaseConfig,
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
--- data-service/src/config/database.config.ts
+++ data-service/src/config/database.config.ts
@@ -1,16 +1,16 @@
-import { ConfigService } from "@nestjs/config";
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { User } from "src/common/entities/user.entity";
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from 'src/common/entities/user.entity';
 
-export const getDatabaseConfig = (ConfigService: ConfigService): TypeOrmModuleOptions => ({
+export const getDatabaseConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
     type: 'mysql',
-    host: ConfigService.get('DB_HOST'),
-    port: ConfigService.get('DB_PORT'),
-    username: ConfigService.get('DB_USERNAME'),
-    password: ConfigService.get('DB_PASSWORD'),
-    database: ConfigService.get('DB_NAME'),
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
     entities: [User], //estáva dessa maneira antes: [__dirname + '/../**/*.entity{.ts,.js}']
     synchronize: true, // de forma alguma deve ser usado em Produção
     logging: true, // como indicação da propria tabnine para o documento eu o coloquei, mas não está ativado porque no documento
     // não existe essa propriedade
-})
+});
